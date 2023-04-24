488e5501026331a88279c9d48bec0f98d4e12871
TheMightyRaider <themightyraider7@gmail.com>
2023-04-24 19:57:36 +0530

    Basic structure setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Graph from "./Components/Main"
+
+class App extends React.Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                <div class="header">
+                    Language Transalation
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class Graph extends React.Component {
+    state = {
+
+    }
+    render() {
+        return "Getting Started!"
+    }
+}
+
+export default Graph
