28ad2d1ff7524a73335fadf10022489bc7cdb4ff
TheMightyRaider <themightyraider7@gmail.com>
2023-05-15 18:12:10 +0530

    Updated backend server with aksharmukha

--- Backend/server.py
+++ Backend/server.py
@@ -1,64 +1,38 @@
 import os
 import sys
-from indicnlp import common
 from flask import Flask, request
-from indicnlp.transliterate.unicode_transliterate import UnicodeIndicTransliterator
+from flask_cors import CORS, cross_origin
+from aksharamukha import transliterate
+from aksharamukha import GeneralMap
 
 # create the Flask app
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-# The path to the local git repo for Indic NLP library
-INDIC_NLP_LIB_HOME=r"indic_nlp_library"
-
-# The path to the local git repo for Indic NLP Resources
-INDIC_NLP_RESOURCES=r"indic_nlp_resources"
-
-SCRIPT_RANGES={
-                "Punjabi":'pa', 
-                "Gujarati":'gu', 
-                "Odia":'or', 
-                "Tamil":'ta', 
-                "Telugu":'te', 
-                "Kannada":'kn', 
-                "Malayalam":'ml', 
-                "Sinhala":'si', 
-                "Hindi":'hi',
-                "Urdu":"hi", 
-                "Marathi":'mr',   
-                "Konkani":'kK',   
-                "Sanskrit":'sa',   
-                "Nepali":'ne',  
-                "Sindhi":'sd', 
-                "Bengali":'bn', 
-                "Assamese":'as'
-              }
-
-# Add library to Python path
-sys.path.append(r'{}\src'.format(INDIC_NLP_LIB_HOME))
-
-# Set environment variable for resources folder
-common.set_resources_path(INDIC_NLP_RESOURCES)
+SCRIPT_LANGUAGES = ["autodetect",*GeneralMap.IndicScripts]
 
 @app.route('/language_code',methods=['GET'])
+@cross_origin()
 def get_language_code():
-    return SCRIPT_RANGES
+    return GeneralMap.IndicScripts
 
 @app.route('/tranliterate',methods=['POST'])
+@cross_origin()
 def tranliterate():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         json = request.get_json()
-        script_from_language=json["from_language"]
-        script_to_language=json["to_language"]
+        from_language=json["from_language"]
+        to_language=json["to_language"]
         sentence=json["sentence"]
 
         try:
-            script_from_language=SCRIPT_RANGES[script_from_language]
-            script_to_language=SCRIPT_RANGES[script_to_language]
-        except:
+            transliterate_word=transliterate.process(from_language,to_language,sentence)
+        except Exception as e:
+            print(e)
             return "Error in the Language Code, Check the language format in /language_code",400
     
-        transliterate_word=UnicodeIndicTransliterator.transliterate(sentence,script_from_language,script_to_language)
         print(transliterate_word)
         return transliterate_word
     else:
@@ -69,4 +43,4 @@ def health():
     return f"Server is up!, Server information ----> {os.uname()}"
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8300,debug=False)
+    app.run(host="0.0.0.0",port=8400,debug=False)
